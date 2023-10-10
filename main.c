33a19a9d83bf52b32b44de26c14ffb89304d4073
aberramo <aberramo@student.42.fr>
2023-10-10 12:27:25 +0200

    save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aberramo <aberramo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 19:28:58 by aberramo          #+#    #+#             */
-/*   Updated: 2023/10/09 19:27:05 by aberramo         ###   ########.fr       */
+/*   Updated: 2023/10/10 12:06:13 by aberramo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int	main(int ac, char **av)
 	la = NULL;
 	lb = NULL;
 	check(&la, &lb, ac, av);
-	// push_swap(&la, &lb, ac - 1);
+	push_swap(&la, &lb, ac - 1);
 }
