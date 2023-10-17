8c0fc7e0d3a6e89363bd8a30eb2a30c0b21ba78c
aberramo <aberramo@student.42.fr>
2023-10-18 01:51:48 +0200

    bug fixed

--- check.c
+++ check.c
@@ -6,7 +6,7 @@
 /*   By: aberramo <aberramo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 19:46:30 by aberramo          #+#    #+#             */
-/*   Updated: 2023/10/18 01:06:57 by aberramo         ###   ########.fr       */
+/*   Updated: 2023/10/18 01:51:13 by aberramo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	check(t_data *data, char **av)
 	if (data->ac == 1)
 		ft_exit(data, EXIT_SUCCESS);
 	parse_la(data, av);
-	if (data->len == 2)
+	if (data->len < 2)
 		ft_exit(data, EXIT_SUCCESS);
 	check_double_vals(data);
 }
